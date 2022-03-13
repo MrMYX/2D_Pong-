def1095a5890a2e214e8ddba98bd9a3ad50676db
KilluasYoYo <98287025+KilluasYoYo@users.noreply.github.com>
2022-03-13 05:41:05 +0300

    Built boundaries for players

--- Pong3x/Assets/Scripts/FirstPlayer.cs
+++ Pong3x/Assets/Scripts/FirstPlayer.cs
@@ -6,14 +6,30 @@ using UnityEngine.InputSystem;
 public class FirstPlayer : MonoBehaviour
 {
     Rigidbody2D myRigidBody;
+    public Transform boundaries;
+
+    Boundary playerBoundary;
+    
+    struct Boundary
+    {
+        public float Up, Down, Left, Right;
+        public Boundary(float up, float down, float left, float right)
+        {
+            Up = up; Down = down; Left = left; Right = right;
+            
+        }
+    }
     Vector2 moveInput;
     [SerializeField] float playerSpeed = 5f;
-    Vector2 minBounds;
-    Vector2 maxBounds;    
+
     void Start()
     {
         myRigidBody = GetComponent<Rigidbody2D>();
 
+        playerBoundary = new Boundary(boundaries.GetChild(0).position.y,
+                                      boundaries.GetChild(1).position.y,
+                                      boundaries.GetChild(2).position.x,
+                                      boundaries.GetChild(3).position.x);
     }
 
     void Update()
@@ -29,5 +45,13 @@ public class FirstPlayer : MonoBehaviour
     {
         Vector3 firstPlayerVelocity = new Vector2(myRigidBody.velocity.y, moveInput.y * playerSpeed);
         myRigidBody.velocity = firstPlayerVelocity;
+
+        Vector2 clampedFirstPlayer = new Vector2(Mathf.Clamp(transform.position.x, 
+                                                             playerBoundary.Left,
+                                                             playerBoundary.Right), 
+                                                 Mathf.Clamp(transform.position.y, 
+                                                             playerBoundary.Down, 
+                                                             playerBoundary.Up));
+        transform.position = clampedFirstPlayer;
     }
 }
--- Pong3x/Assets/Scripts/SecondPlayer.cs
+++ Pong3x/Assets/Scripts/SecondPlayer.cs
@@ -8,9 +8,27 @@ public class SecondPlayer : MonoBehaviour
     Vector2 moveInput;
     [SerializeField] float secondPlayerMoveSpeed = 5f;
     Rigidbody2D myrigidbody;
+    public Transform boundaries;
+
+    Boundary playerBoundary;
+
+    struct Boundary
+    {
+        public float Up, Down, Left, Right;
+        public Boundary(float up, float down, float left, float right)
+        {
+            Up = up; Down = down; Left = left; Right = right;
+ 
+        }
+    }
+
     void Start()
     {
         myrigidbody = GetComponent<Rigidbody2D>();
+        playerBoundary = new Boundary(boundaries.GetChild(0).position.y,
+                                      boundaries.GetChild(1).position.y,
+                                      boundaries.GetChild(2).position.x,
+                                      boundaries.GetChild(3).position.x);
     }
 
     void Update()
@@ -27,5 +45,13 @@ public class SecondPlayer : MonoBehaviour
     {
         Vector2 secondPlayerVelocity = new Vector2(myrigidbody.velocity.x, moveInput.y * secondPlayerMoveSpeed);
         myrigidbody.velocity = secondPlayerVelocity;
+
+        Vector2 clampedSecondPlayer = new Vector2(Mathf.Clamp(transform.position.x, 
+                                                             playerBoundary.Left,
+                                                             playerBoundary.Right), 
+                                                 Mathf.Clamp(transform.position.y, 
+                                                             playerBoundary.Down, 
+                                                             playerBoundary.Up));
+        transform.position = clampedSecondPlayer;
     }
 }
